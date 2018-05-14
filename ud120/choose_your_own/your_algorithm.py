12b9f39dd71b379fba2dc20018507b1f68811e1f
“zhaoxuyan” <526161421@qq.com>
2018-05-14 12:01:53 +0800

    update 选择自己的算法Adaboost

--- ud120/choose_your_own/your_algorithm.py
+++ ud120/choose_your_own/your_algorithm.py
@@ -6,21 +6,19 @@ from class_vis import prettyPicture
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
-
 ### the training data (features_train, labels_train) have both "fast" and "slow"
 ### points mixed together--separate them so we can give them different colors
 ### in the scatterplot and identify them visually
-grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii] == 1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii] == 1]
 
 #### initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.scatter(bumpy_fast, grade_fast, color="b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color="r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
@@ -30,13 +28,16 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 
-
-
-
-
-
-
+clf = AdaBoostClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print "accuracy_score:", accuracy_score(pred, labels_test)
+scores = cross_val_score(clf, features_train, labels_train)
+print "cross_val_score:", scores.mean()
 
 try:
     prettyPicture(clf, features_test, labels_test)
--- ud120/tools/email_preprocess.py
+++ ud120/tools/email_preprocess.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 import pickle
 import cPickle
@@ -49,7 +50,8 @@ def preprocess(words_file="../tools/word_data.pkl", authors_file="../tools/email
 
     ### feature selection, because text is super high dimensional and
     ### can be really computationally chewy as a result
-    selector = SelectPercentile(f_classif, percentile=10)
+    # percentile改为1，则特征数减少10倍
+    selector = SelectPercentile(f_classif, percentile=1)
     selector.fit(features_train_transformed, labels_train)
     features_train_transformed = selector.transform(features_train_transformed).toarray()
     features_test_transformed = selector.transform(features_test_transformed).toarray()
