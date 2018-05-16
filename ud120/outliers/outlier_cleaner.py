74208eeadbddb5f855404348b2a636e0daf3bbd7
“zhaoxuyan” <526161421@qq.com>
2018-05-17 01:19:07 +0800

    完成异常值处理

--- ud120/outliers/outlier_cleaner.py
+++ ud120/outliers/outlier_cleaner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 
 def outlierCleaner(predictions, ages, net_worths):
@@ -10,11 +11,24 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
-
+    err = []
+    temp = []
     ### your code goes here
+    for i in range(len(predictions)):
+        # err = numpy.fabs(net_worths - predicitons)
+        err.append(abs(net_worths[i] - predictions[i]))
+        temp.append((ages[i], net_worths[i], err[i]))
 
-    
+    # 从小到大排序
+    err.sort()
+    err = err[:81]
+
+
+    for j in range(len(temp)):
+        if temp[j][2] in err:
+            cleaned_data.append(temp[j])
+
     return cleaned_data
 
