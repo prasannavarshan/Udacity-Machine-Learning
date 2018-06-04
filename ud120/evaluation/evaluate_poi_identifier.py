c1d74756a5b87c3c5aa8bed7109e4d77970e4dc5
“zhaoxuyan” <526161421@qq.com>
2018-06-05 00:39:08 +0800

    update 模型的评估与验证

--- ud120/evaluation/evaluate_poi_identifier.py
+++ ud120/evaluation/evaluate_poi_identifier.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 
 """
@@ -13,10 +14,15 @@
 
 import pickle
 import sys
+
+from sklearn.metrics import precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
@@ -24,8 +30,47 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### your code goes here
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3,
+                                                                            random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "accuracy score:", clf.score(features_test, labels_test)
+
+# How many POIs are in the test set for your POI identifier?
+# 测试集中有多少 POI？
+pred = clf.predict(features_test)
+
+print len([e for e in labels_test if e == 1.0])
+
+# How many people total are in your test set?
+print "测试集总人数:", len(labels_test)
+
+# Precision and recall can help illuminate your performance better.
+# Use the precision_score and recall_score available in sklearn.metrics to compute those quantities.
+
+# What’s the precision?
 
 
-### your code goes here 
+print "precision_score", precision_score(labels_test, pred)
+
+# What’s the recall?
+print "recall_score", recall_score(labels_test, pred)
+
+# Here are some made-up predictions and true labels for a hypothetical test set; fill in the following boxes to
+# practice identifying true positives, false positives, true negatives, and false negatives. Let’s use the convention
+#  that “1” signifies a positive result, and “0” a negative.
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+print len([e for e in true_labels if e == 0])
 
 
+
+
+
+# What's the precision of this classifier?
+print precision_score(true_labels, predictions)
+
+# What's the recall of this classifier?
+print recall_score(true_labels, predictions)
--- ud120/validation/validate_poi.py
+++ ud120/validation/validate_poi.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 
 """
@@ -12,10 +13,13 @@
 
 import pickle
 import sys
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -25,8 +29,20 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### it's all yours from here forward!
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
+print clf.score(features, labels)
+# 0.98958333333333337
 
+# Now you’ll add in training and testing, so that you get a trustworthy accuracy number.
+# Use the training_test_split validation available in sklearn.cross_validation
 
-### it's all yours from here forward!  
+features_train, features_test, labels_train, labels_test = train_test_split(features,
+                                                                            labels, test_size=0.3,
+                                                                            random_state=42)
 
-
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
+# 0.72413793103448276
